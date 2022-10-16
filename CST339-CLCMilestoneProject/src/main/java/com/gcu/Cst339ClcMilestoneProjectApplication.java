15da813eb43108423c1dc7a689245373d129c1a1
jtlewis81 <jtlewis91481@gmail.com>
2022-10-16 00:56:30 -0400

    Initial commit

---
+++ CST339-CLCMilestoneProject/src/main/java/com/gcu/Cst339ClcMilestoneProjectApplication.java
@@ -0,0 +1,14 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst339ClcMilestoneProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello there!");
+		SpringApplication.run(Cst339ClcMilestoneProjectApplication.class, args);
+	}
+
+}
