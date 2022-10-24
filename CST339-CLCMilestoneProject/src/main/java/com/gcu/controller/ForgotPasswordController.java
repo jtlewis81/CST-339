3a0117998d2550bdf92cee8ec24da0d1a34ce99d
Jugooch <justicegooch@hotmail.com>
2022-10-23 18:35:40 -0700

    ForgotPassword added *not implemented*

---
+++ CST339-CLCMilestoneProject/src/main/java/com/gcu/controller/ForgotPasswordController.java
@@ -0,0 +1,25 @@
+package com.gcu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/forgot-password")
+public class ForgotPasswordController {
+
+    /**
+     * Display 
+     * 
+     * @param model
+     * @return
+     */
+    @GetMapping("/")
+    public String display(Model model) 
+    {
+        model.addAttribute("title", "Forgot Password");    
+        model.addAttribute("pageName", "Forgot Password");
+        return "forgot-password";
+    }
+}
